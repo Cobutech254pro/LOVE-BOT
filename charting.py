55d2027bd4f89343a0fe1827a80302df9743480b
Cobutech254pro <calvinonyango963@gmail.com>
2025-05-05 19:35:24 +0300

    𝐏𝐑𝐄𝐌𝐈𝐔𝐌

---
+++ charting.py
@@ -0,0 +1,7 @@
+def handle_relationship_query(query):
+    print(f"Thank you for sharing. We are processing your relationship inquiry: '{query}'")
+    # In the future, this function will generate charts based on the query.
+    pass
+
+if __name__ == '__main__':
+    handle_relationship_query("I'm curious about how our communication patterns have changed over time.")
